7f7cbdf2d26ece42c01d2efb7ffc7b47d4717a52
Juan Camargo <juancamargo93@outlook.com>
2022-11-22 11:10:15 -0500

    add logic to get all contracts belonging to a user (client or contractor)

--- src/app.js
+++ src/app.js
@@ -2,20 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+const {Op} = require('sequelize');
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
+app.get('/contracts/:id', getProfile, async (req, res) =>{
     const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: { id, ClientId: req.profile.id }});
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+    const {id} = req.params;
+    const contract = await Contract.findOne({where: { id, ContractorId: req.profile.id }});
+    if(!contract) return res.status(404).end();
+    res.json(contract);
+});
+
+/**
+ * @returns contracts belonging to a user
+ */
+
+app.get('/contracts/', getProfile , async (req, res) =>{
+    const {Contract} = req.app.get('models');
+    const isNotTerminated = 'terminated';
+    try {
+        const contracts = await Contract.findAll({where: {
+            status: { [Op.not]: isNotTerminated },
+            [Op.or]: [
+                { ContractorId: req.profile.id },
+                { ClientId: req.profile.id }
+            ]
+        }});
+        if(!contracts) return res.status(404).end();
+        res.json(contracts);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 module.exports = app;
