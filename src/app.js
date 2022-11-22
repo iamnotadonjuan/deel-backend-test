a78424b09ee6c7c2b6e52baae7b55043db810ccf
Juan Camargo <juancamargo93@outlook.com>
2022-11-22 15:30:14 -0500

    add best clients new feature

--- src/app.js
+++ src/app.js
@@ -180,4 +180,43 @@ app.get('/admin/best-profession', getProfile , async (req, res) =>{
     }
 });
 
+app.get('/admin/best-clients', getProfile , async (req, res) =>{
+
+    //contractor jobs paid
+    const {Profile, Contract, Job} = req.app.get('models');
+    const {start,end,limit} = req.query;
+    const isAClient = 'client';
+    if (!start && !end) return res.status(400).end();
+    try {
+        const bestProfession = await Profile.findAll({
+            where: {
+                type: { [Op.eq]: isAClient }
+            },
+            limit: limit || 2,
+            include: [{
+                model: Contract,
+                as: 'Client',
+                include: [{
+                    model: Job,
+                    where: {
+                        paid: 1,
+                        paymentDate: { [Op.between]: [start, end] }
+                    }
+                }]
+            }]
+        });
+        const totalJobsToPay = bestProfession.map((bestP => ({
+            ...bestP.dataValues,
+            paid: bestP.Client.map((client) => client.Jobs.reduce((acc, obj) => acc + obj.price, 0)).reduce((c, v) => c + v, 0)
+            })
+        )).sort((a, b) => a.paid - b.paid);
+        
+        if(!totalJobsToPay) return res.status(404).end();
+
+        res.json(totalJobsToPay);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 module.exports = app;
